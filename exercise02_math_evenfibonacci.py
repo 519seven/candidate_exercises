dd0eb50af392e8628acd7d73bb668d819c19bc73
519seven_mbp <ideal.pen2570@fastmail.com>
2022-12-21 13:32:54 -0500

    Add math even fibonacci

---
+++ exercise02_math_evenfibonacci.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+#   Series 2
+
+# # Topic
+# ## Math
+
+# # Challenge Level
+# ## Easy
+
+# # Description
+#
+# # ProjectEuler.com Problem 2 
+#   Find the sum of all the even Fibonacci numbers
+#   less than 4 million.
+#
+# # Your Solutions' Relative Levels:
+#
+# - *Easy/Beginner*: for loop(s)
+# - *Medium/Intermediate*: list comprehension
+# - *Hard/Expert*: NumPy
+#
+# # Example Use Case(s)
+#
+# - A Fibonacci sequence involves adding the previous value
+#   to the current value. Examples:
+#   Sequence 1 - 4 --> 1 2 3 4 --> 3 3 4 --> 6 4 --> 10 
+# ******************************************************************************
+
+MAX_NUM = 4000000
+
+for i in range(1, MAX_NUM):
+    pass
