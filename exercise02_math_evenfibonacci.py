6e79a146251c9b0e4bb504e0baa4c147e597a137
519seven_mbp <ideal.pen2570@fastmail.com>
2023-01-05 20:49:19 -0500

    Reformat presentation of exercise

--- exercise02_math_evenfibonacci.py
+++ exercise02_math_evenfibonacci.py
@@ -11,23 +11,32 @@
 # # Description
 #
 # # ProjectEuler.com Problem 2 
-#   Find the sum of all the even Fibonacci numbers
-#   less than 4 million.
+#   Find the sum of all the even Fibonacci numbers that are less than 4 million.
+#   Fibonacci explained: https://www.calculatorsoup.com/calculators/discretemathematics/fibonacci-calculator.php
 #
 # # Your Solutions' Relative Levels:
-#
-# - *Easy/Beginner*: for loop(s)
-# - *Medium/Intermediate*: list comprehension
-# - *Hard/Expert*: NumPy
+#   Easyu: while loop
+#   Intermediate: using py-fibonacci (https://pypi.org/project/py-fibonacci/)
+#   Hard: create your own library
 #
 # # Example Use Case(s)
 #
-# - A Fibonacci sequence involves adding the previous value
-#   to the current value. Examples:
-#   Sequence 1 - 4 --> 1 2 3 4 --> 3 3 4 --> 6 4 --> 10 
+# - A Fibonacci sequence involves adding the previous number to the current 
+#   number, starting with 0 and 1.
+#   Example: 0 1 1 2 3 5 8 13 21 34 55 89
 # ******************************************************************************
 
-MAX_NUM = 4000000
+# initialize variables
+MAX_NUM = 4e6          # 4000000
+CUMULATIVE_SUM = 0     # track the sum of even numbers
 
-for i in range(1, MAX_NUM):
-    pass
+a, b = 0, 1
+
+# add your solution to update CUMULATIVE_SUM
+
+if CUMULATIVE_SUM > 0:
+    print(f"The sum of numbers of a Fibonacci sequence that are even and less than {int(MAX_NUM)} is {CUMULATIVE_SUM}")
+print("The End.")
+
+import sys
+sys.exit(0)
